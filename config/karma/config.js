d8ab1be14dc3271fcc136007c5d5602cd5b15fd1
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-13 23:25:37 +0200

    run tests on BrowserStack

--- config/karma/config.js
+++ config/karma/config.js
@@ -41,23 +41,30 @@ module.exports = (config) => {
 
         config.set({
 
+            browserStack: {
+                accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
+                username: process.env.BROWSER_STACK_USERNAME
+            },
+
             browsers: [
-                'ChromeSauceLabs',
-                'FirefoxSauceLabs'
+                'ChromeBrowserStack',
+                'FirefoxBrowserStack'
             ],
 
             captureTimeout: 120000,
 
             customLaunchers: {
-                ChromeSauceLabs: {
-                    base: 'SauceLabs',
-                    browserName: 'chrome',
-                    platform: 'OS X 10.11'
+                ChromeBrowserStack: {
+                    base: 'BrowserStack',
+                    browser: 'chrome',
+                    os: 'OS X',
+                    os_version: 'Sierra' // eslint-disable-line camelcase
                 },
-                FirefoxSauceLabs: {
-                    base: 'SauceLabs',
-                    browserName: 'firefox',
-                    platform: 'OS X 10.11'
+                FirefoxBrowserStack: {
+                    base: 'BrowserStack',
+                    browser: 'firefox',
+                    os: 'OS X',
+                    os_version: 'Sierra' // eslint-disable-line camelcase
                 }
             },
 
