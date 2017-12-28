9d764d16db9c09753f7e887fbe527ae9b5566d72
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-28 16:07:32 +0100

    define type of exported wavEncoder

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,8 @@
-import { load } from 'extendable-media-recorder-wav-encoder-broker';
+import { IWavEncoder, load } from 'extendable-media-recorder-wav-encoder-broker';
 import { worker } from './worker/worker';
 
 const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
 
 const url: string = URL.createObjectURL(blob);
 
-export const wavEncoder = load(url);
+export const wavEncoder: IWavEncoder = load(url);
