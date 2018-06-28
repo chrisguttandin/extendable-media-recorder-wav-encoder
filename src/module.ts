2cb9f9b02c95f818a505c605eb5f5772284618dd
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-06-28 22:02:37 +0200

    fix: add explicit type declaration

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { load } from 'extendable-media-recorder-wav-encoder-broker';
+import { IExtendableMediaRecorderWavEncoderBrokerDefinition, load } from 'extendable-media-recorder-wav-encoder-broker';
 import { worker } from './worker/worker';
 
 const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
@@ -15,4 +15,5 @@ export const disconnect = extendableMediaRecorderWavEncoder.disconnect;
 
 export const encode = extendableMediaRecorderWavEncoder.encode;
 
-export const record = extendableMediaRecorderWavEncoder.record;
+// @todo TypeScript v2.9.2 requires record to have an explicit type.
+export const record: IExtendableMediaRecorderWavEncoderBrokerDefinition['record'] = extendableMediaRecorderWavEncoder.record;
