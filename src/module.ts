6f15b758c328909d5749c8af5ae62f2514e23a4d
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-08-28 01:47:16 +0200

    fix: remove inferable types

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { IExtendableMediaRecorderWavEncoderBrokerDefinition, load } from 'extendable-media-recorder-wav-encoder-broker';
+import { load } from 'extendable-media-recorder-wav-encoder-broker';
 import { worker } from './worker/worker';
 
 const blob: Blob = new Blob([ worker ], { type: 'application/javascript; charset=utf-8' });
@@ -15,7 +15,6 @@ export const disconnect = extendableMediaRecorderWavEncoder.disconnect;
 
 export const encode = extendableMediaRecorderWavEncoder.encode;
 
-// @todo TypeScript v2.9.2 requires record to have an explicit type.
-export const record: IExtendableMediaRecorderWavEncoderBrokerDefinition['record'] = extendableMediaRecorderWavEncoder.record;
+export const record = extendableMediaRecorderWavEncoder.record;
 
 URL.revokeObjectURL(url);
