e4253d3a974bacb8e7a6e563e3739f72e8095a38
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-05-29 23:36:34 +0200

    test(config): run tests in Firefox on BrowserStack on Windows

--- config/karma/config.js
+++ config/karma/config.js
@@ -66,8 +66,8 @@ module.exports = (config) => {
                 FirefoxBrowserStack: {
                     base: 'BrowserStack',
                     browser: 'firefox',
-                    os: 'OS X',
-                    os_version: 'High Sierra' // eslint-disable-line camelcase
+                    os: 'Windows',
+                    os_version: '10' // eslint-disable-line camelcase
                 }
             },
 
